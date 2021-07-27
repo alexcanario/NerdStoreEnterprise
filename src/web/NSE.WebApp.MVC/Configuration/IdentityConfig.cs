e7f6fa6aa50b9c95191600952725cdb554a8f8d0
Alex Canario <alexcanario@gmail.com>
2021-07-27 20:04:53 -0300

    4.3 Adicionar autenticação por Cookie, close #18

---
+++ src/web/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Configuration {
+    public static class IdentityConfig {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services) {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfig(this IApplicationBuilder app) {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app.UseIdentityConfig();
+        }
+    }
+}
