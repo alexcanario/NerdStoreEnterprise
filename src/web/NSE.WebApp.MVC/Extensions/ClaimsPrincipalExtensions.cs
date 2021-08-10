17ba196466a523b8a03f1f46b16d597ed505522e
Alex Canario <alexcanario@gmail.com>
2021-08-09 22:04:55 -0300

    4.8 Implementar uma forma de obter dados do usuari; close #25

---
+++ src/web/NSE.WebApp.MVC/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions {
+    public static class ClaimsPrincipalExtensions {
+        public static string ObterUserId(this ClaimsPrincipal principal) {
+            if(principal.Equals(null)) throw new ArgumentNullException(nameof(principal));
+
+            var claim = principal.FindFirst("sub");
+            return claim?.Value;
+        }
+
+        public static string ObterUserEmail(this ClaimsPrincipal principal) {
+            if(principal.Equals(null)) throw new ArgumentNullException(nameof(principal));
+
+            var claim = principal.FindFirst("email");
+            return claim?.Value;
+        }
+
+        public static string ObterUserToken(this ClaimsPrincipal principal) {
+            if(principal.Equals(null)) throw new ArgumentNullException(nameof(principal));
+
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions {
+    public interface IUser {
+        string Nome { get; }
+        Guid ObterUserId();
+        string ObterUserEmail();
+        string ObterUserToken();
+        bool EstaAutenticado();
+        bool PossuiRole(string role);
+        IEnumerable<Claim> ObterClaim();
+        HttpContext ObterHttpContext();
+    }
+}
