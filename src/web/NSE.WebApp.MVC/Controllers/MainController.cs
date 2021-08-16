c03dbecbbe9a6e3a15ac40724d23c9b5b42ba655
Alex Canario <alexcanario@gmail.com>
2021-08-16 13:36:18 -0300

    4.9 Tratamento de erros do Response

---
+++ src/web/NSE.WebApp.MVC/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using NSE.WebApp.MVC.Models;
+
+using System.Linq;
+
+namespace NSE.WebApp.MVC.Controllers {
+    public class MainController : Controller {
+        protected bool ResponsePossuiErros(ResponseResult response) {
+            return (response is not null && response.Errors.Mensagens.Any());
+        }
+
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace NSE.WebApp.MVC.Services {
+    public abstract class Service {
+        protected bool TratarErrosResponse(HttpResponseMessage response) {
+            switch (response.StatusCode) {
+                case HttpStatusCode.Unauthorized:   //401
+                case HttpStatusCode.Forbidden:      //403
+                case HttpStatusCode.NotFound:       //404
+                case HttpStatusCode.InternalServerError: //500
+                    throw new Exception();
+
+                case HttpStatusCode.BadRequest:     //400
+                    return false;
+            }
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}
