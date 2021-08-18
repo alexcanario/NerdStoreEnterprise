789865921e6d05db2d3f717c2e6502d60fe2bd85
Alex Canario <alexcanario@gmail.com>
2021-08-18 00:35:55 -0300

    4.10 Mostrar erros de logon ao usuário, implementar o logout

--- src/web/NSE.WebApp.MVC/Controllers/MainController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/MainController.cs
@@ -7,7 +7,15 @@ using System.Linq;
 namespace NSE.WebApp.MVC.Controllers {
     public class MainController : Controller {
         protected bool ResponsePossuiErros(ResponseResult response) {
-            return (response is not null && response.Errors.Mensagens.Any());
+            if (response is not null && response.Errors.Mensagens.Any()) {
+                //response.Errors.Mensagens.ForEach(mensagem => { ModelState.AddModelError("Response error: ", mensagem); });
+                foreach(var msg in response.Errors.Mensagens) { 
+                    ModelState.AddModelError(string.Empty, msg);
+                }
+                return true;
+            }
+
+            return false;
         }
 
     }
