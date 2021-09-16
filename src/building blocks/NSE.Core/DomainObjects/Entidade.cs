9035b0d7d5cdd1937dd07f63888659e3a16daa81
Alex Canario <alexcanario@gmail.com>
2021-09-16 16:42:35 -0300

    Aula Acesso a dados, na api catalogo

---
+++ src/building blocks/NSE.Core/DomainObjects/Entidade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NSE.Core.DomainObjects {
+    public abstract class Entidade {
+        public Guid Id { get; set; }
+
+        public override bool Equals(object obj) {
+            var compareTo = obj as Entidade;
+            
+            if(ReferenceEquals(this, compareTo)) return true;
+            if(ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public override string ToString() {
+            return $"{GetType().Name} [Id: {Id}]";
+        }
+
+        public static bool operator !=(Entidade a, Entidade b) {
+            return !(a == b);
+        }
+
+        public static bool operator ==(Entidade a, Entidade b) { 
+            return a.Equals(b);
+        }
+
+        public override int GetHashCode() {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+    }
+}
